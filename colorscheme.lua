f0c101730187eb1dc8e8f57bda9c613cf4ff7a2a
DESKTOP-5U6983D\ShenYi <shenyisammi@gmail.com>
2023-06-09 03:15:15 +0800

    add theme

---
+++ colorscheme.lua
@@ -0,0 +1,5 @@
+return {
+  "rose-pine",
+  --"catppuccin",
+  --"astrodark",
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,31 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  {
+    "rose-pine",
+    opts = {
+      variant = 'moon',
+      dark_variant = 'main',
+    }
+  }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
