aa941aa5ff0de5e8439221ba480292c54fc0e2e2
DESKTOP-5U6983D\ShenYi <shenyisammi@gmail.com>
2023-06-09 13:03:31 +0800

    add lua lsp stylelint, seprate updater.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  -- language packer
+  { import = "astrocommunity.pack.lua" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.colorscheme.catppuccin" },
